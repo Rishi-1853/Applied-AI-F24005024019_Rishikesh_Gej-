78999e6568dd3181a05a932c3bfc2ced815952c0
Rishi-1853 <rishigej72@gmail.com>
2026-06-02 15:27:10 +0530

    Add files via upload

---
+++ Day_03_Average.py
@@ -0,0 +1,8 @@
+a= int(input("Enter 1st Number: "))
+b= int(input("Enter 2nd Number: "))
+c= int(input("Enter 3rd Number: "))
+d= int(input("Enter 4th Number: "))
+
+Avg= a+b+c+d/4
+
+print("The Average of Four Numbers is: ", Avg)
