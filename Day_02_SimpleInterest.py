36fd5570218eebeffb25ccf3cee73196922782a9
Rishi-1853 <rishigej72@gmail.com>
2026-06-02 16:39:22 +0530

    Add files via upload

---
+++ Day_02_SimpleInterest.py
@@ -0,0 +1,7 @@
+Principal= float(input("Enter the Principal Amount: "))
+Rate= float(input("Enter the Rate of Interest (%): "))
+Time= float(input("Enter the Time (in Years): "))
+
+Simple_Interest= (Principal * Rate * Time) / 100
+
+print("Simple Interest= ", Simple_Interest)
